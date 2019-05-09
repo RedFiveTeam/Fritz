46a7d53774bf96b431f5ee1bb3d3928243c4f3ba
Lucas <Jack@BlackPearl.io>
2019-05-09 14:52:09 -0400

    Finished
    Former-commit-id: 415eb52027584d669dad4be631a98f90aeabe330

--- scripts/build_jar.sh
+++ scripts/build_jar.sh
@@ -11,4 +11,6 @@ popd
 
 pushd ${BASE_DIR}
     mvn -Dflyway.user=${FRITZ_DB_USERNAME} -Dflyway.password= -Dflyway.url=${FRITZ_DB_URL} clean flyway:migrate package -DskipTests
+    rm ${BASE_DIR}/artifacts/fritz.jar || true
+    cp ${BASE_DIR}/target/fritz-[0-9\.]*-SNAPSHOT.jar ${BASE_DIR}/artifacts/fritz.jar
 popd

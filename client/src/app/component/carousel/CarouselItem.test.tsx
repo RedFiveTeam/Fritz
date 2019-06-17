d3f18c6c015e3106abdb0dcda65074f6daba58f4
Jason Rimer <jasonmrimer@gmail.com>
2019-06-17 10:30:41 -0400

    Fritz does not tab to the **first empty text input** in the carousel [#166640306]
    Former-commit-id: c22c227a033c8375082fb9515567b8c8ad98f750

--- client/src/app/component/carousel/CarouselItem.test.tsx
+++ client/src/app/component/carousel/CarouselItem.test.tsx
@@ -1,18 +1,32 @@
 import * as React from 'react';
-import { shallow, ShallowWrapper } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 import { CarouselItem } from './CarouselItem';
 import { SlideModel } from '../slides/models/SlideModel';
 import { StyledValidatingInput } from '../input/ValidatingInput';
-import Mock = jest.Mock;
 import { SlidesStore } from '../slides/store/SlidesStore';
 import { StyledDatePicker } from '../date/DatePicker';
+import Mock = jest.Mock;
 
 describe('CarouselItem', () => {
-  let subject: ShallowWrapper;
+  let subject: ReactWrapper;
   let slide: SlideModel;
   let changeTimeSpy: Mock;
   let changeActivitySpy: Mock;
   let slidesStore: SlidesStore;
+  let carouselActions: any;
+
+  let mountFirst = () => {
+    subject = mount(
+      <CarouselItem
+        active={true}
+        slide={slide}
+        changeTime={changeTimeSpy}
+        changeActivity={changeActivitySpy}
+        slidesStore={slidesStore}
+        carouselActions={carouselActions}
+      />
+    );
+  };
 
   beforeEach(() => {
     slide = new SlideModel('imagePath', 'test Slide', '1234', 'TESTACTY', false, '', '');
@@ -21,19 +35,21 @@ describe('CarouselItem', () => {
     changeActivitySpy = jest.fn();
 
     slidesStore = new SlidesStore();
+    slide.setDate(new Date(Date.now()));
 
-    subject = shallow(
+    subject = mount(
       <CarouselItem
         slide={slide}
         changeTime={changeTimeSpy}
         changeActivity={changeActivitySpy}
         slidesStore={slidesStore}
+        carouselActions={carouselActions}
       />
     );
   });
 
   it('should display slide image', () => {
-    expect(subject.find('img').prop('src')).toBe('api/image/hash/imagePath');
+    expect(subject.find('img').at(0).prop('src')).toBe('api/image/hash/imagePath');
   });
 
   it('should display a time input', () => {
@@ -58,5 +74,19 @@ describe('CarouselItem', () => {
 
   it('should display a date picker that increments or decrements the date', () => {
     expect(subject.find(StyledDatePicker).exists()).toBeTruthy();
+  });
+
+  it('should focus on the time input if empty', () => {
+    slide.setTime('');
+    mountFirst();
+    let timeInput = subject.find('input').at(0).instance();
+    expect(timeInput).toBe(document.activeElement);
+  });
+
+  it('should focus on the activity input if the time input is valid', () => {
+    slide.setTime('1234');
+    mountFirst();
+    let activityInput = subject.find('input').at(1).instance();
+    expect(activityInput).toBe(document.activeElement);
   });
 });

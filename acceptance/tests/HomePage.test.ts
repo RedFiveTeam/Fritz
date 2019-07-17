f119cc1b2fa8fd2c96a9ace9847a31f3d367b602
Jason Rimer <jasonmrimer@gmail.com>
2019-07-17 10:04:26 -0400

    Changed tests from waitForText to assert/grabTextFrom
    Former-commit-id: 5a8b6eb85d687b76c5c12bb398155f10482668f6

--- acceptance/tests/HomePage.test.ts
+++ acceptance/tests/HomePage.test.ts
@@ -28,6 +28,7 @@ Scenario('should allow you to edit the activity and time of a image and view ima
 Scenario('should edit slide information in the carousel', (I) => {
   navigateHomeAndUploadPDF(I);
 
+  I.waitForText('UNICORN Callout', 10);
   I.click('.thumbnailClickOverlay');
   assert(
     I.grabTextFrom('.currentSlide > .slideTitle'),
@@ -49,7 +50,7 @@ Scenario('should edit slide information in the carousel', (I) => {
     I.grabTextFrom('.slideTitle'),
     '300123ZAPR19_OP_LEPRECHAUN_PHASE_8_CAROUSEL_ACTIVITY_TEST_STEPHEN_13_RELEASABILITY'
   );
-  I.waitForText('JPEG Renamer - Details', 10);
+  assert(I.grabTextFrom('.header > h2'), 'JPEG Renamer - Details');
 });
 
 Scenario('should restrict uploads to PDFs and delete uploads', (I) => {
@@ -58,12 +59,14 @@ Scenario('should restrict uploads to PDFs and delete uploads', (I) => {
   I.attachFile('#browseInput', 'data/blank.txt');
   I.waitForText('File must be a PDF', 10);
   I.attachFile('#browseInput', 'data/AcceptanceMission.pdf');
-  I.waitForText('AcceptanceMission.pdf', 10);
+  assert(
+    I.grabTextFrom('#pdfFileName'), 'AcceptanceMission.pdf');
 
   assert(
     I.grabTextFrom('.slideTitle'),
     '291235ZAPR19_OP_LEPRECHAUN_PHASE_8_ACTY_STEPHEN_13_RELEASABILITY'
   );
+  I.waitForText('UNICORN Callout', 10);
   I.click('#deletePP');
   I.waitForText('Are you sure you want to delete the PDF', 10);
   I.click('.btn-primary');
@@ -74,8 +77,8 @@ Scenario('should restrict uploads to PDFs and delete uploads', (I) => {
 Scenario('should validateInput fields before download', (I) => {
   navigateHomeAndUploadPDF(I);
 
-  I.waitForText('The callsign does not match selected mission', 5);
-  I.waitForText('The releasability field must be chosen', 5);
+  assert(I.grabTextFrom('.validatingInput:nth-of-type(2) > .errorMessage'), 'The callsign does not match selected mission');
+  assert(I.grabTextFrom('.controlUnit > .errorMessage'), 'The releasability field must be chosen');
 
   I.fillField('#opInput', 'd');
   I.pressKey('Backspace');
@@ -88,23 +91,23 @@ Scenario('should validateInput fields before download', (I) => {
   I.dontSee('The callsign field must not be empty');
 
   I.click('#downloadButton');
-  I.waitForText('The op name field must not be empty', 5);
-  I.waitForText('The callsign field must not be empty', 5);
-  I.waitForText('The releasability field must be chosen', 5);
+  assert(I.grabTextFrom('.validatingInput:first-of-type > .errorMessage'), 'The op name field must not be empty');
+  assert(I.grabTextFrom('.validatingInput:nth-of-type(2) > .errorMessage'), 'The callsign field must not be empty');
+  assert(I.grabTextFrom('.controlUnit > .errorMessage'), 'The releasability field must be chosen');
 
   I.fillField('#opInput', 'op test');
-  I.dontSee('The op name field must not be empty');
-  I.waitForText('The callsign field must not be empty', 5);
-  I.waitForText('The releasability field must be chosen', 5);
+  assert(I.grabTextFrom('.validatingInput:first-of-type > .errorMessage'), 'The op name field must not be empty');
+  assert(I.grabTextFrom('.validatingInput:nth-of-type(2) > .errorMessage'), 'The callsign field must not be empty');
+  assert(I.grabTextFrom('.controlUnit > .errorMessage'), 'The releasability field must be chosen');
 
   I.fillField('#assetInput', 'asset');
   I.dontSee('The callsign field must not be empty');
-  I.waitForText('The callsign does not match selected mission', 5);
-  I.waitForText('The releasability field must be chosen', 5);
+  assert(I.grabTextFrom('.validatingInput:nth-of-type(2) > .errorMessage'), 'The callsign does not match selected mission');
+  assert(I.grabTextFrom('.controlUnit > .errorMessage'), 'The releasability field must be chosen');
 
   I.fillField('#assetInput', 'TEST11');
   I.dontSee('The callsign does not match selected mission');
-  I.waitForText('The releasability field must be chosen', 5);
+  assert(I.grabTextFrom('.controlUnit > .errorMessage'), 'The releasability field must be chosen');
 
   I.click('FOUO');
   I.dontSee('The releasability field must be chosen');
@@ -122,7 +125,8 @@ function navigateHomeAndSelectMission(I) {
 function navigateHomeAndUploadPDF(I) {
   navigateHomeAndSelectMission(I);
   I.attachFile('#browseInput', 'data/AcceptanceMission.pdf');
-  I.waitForText('AcceptanceMission.pdf', 10);
+  assert(
+  I.grabTextFrom('#pdfFileName'), 'AcceptanceMission.pdf');
   assert(
     I.grabTextFrom('.slideTitle'),
     '291235ZAPR19_OP_LEPRECHAUN_PHASE_8_ACTY_STEPHEN_13_RELEASABILITY'
